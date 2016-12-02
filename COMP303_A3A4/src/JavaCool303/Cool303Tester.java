745fb80ddd8799b21d05b2eedb56bd7d5b7da276
anuragkalra <anuragkalra@outlook.com>
2016-12-02 18:14:54 -0500

    test

--- COMP303_A3A4/src/JavaCool303/Cool303Tester.java
+++ COMP303_A3A4/src/JavaCool303/Cool303Tester.java
@@ -1,5 +1,13 @@
 package JavaCool303;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.border.EtchedBorder;
+
 /**
  * 
  * @author Anurag Kalra
@@ -10,7 +18,32 @@ package JavaCool303;
 public class Cool303Tester {
 
 	public static void main(String[] args) {
+		JButton button1 = new JButton("test");
+		button1.setBackground(Color.GREEN);
+		button1.setForeground(Color.RED);
+		button1.setBorder(new EtchedBorder());
 		
+		JButton button2 = new JButton("test");
+		button2.setBackground(Color.ORANGE);
+		button2.setForeground(Color.BLUE);
+		button2.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		
+		JButton button3 = new JButton("test");
+		button3.setBackground(Color.YELLOW);
+		button3.setForeground(Color.MAGENTA);
+		button3.setBorder(BorderFactory.createMatteBorder(1, 5, 1, 5, Color.red));
+		
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		
+		frame.add(button1);
+		frame.add(button2);
+		frame.add(button3);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
